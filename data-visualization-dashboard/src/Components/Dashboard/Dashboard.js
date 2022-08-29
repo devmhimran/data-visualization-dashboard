b45dbe8566c08ce8813836d881809832842c2fc1
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-08-29 20:43:23 +0600

    Update Dashboard & Navbar

--- data-visualization-dashboard/src/Components/Dashboard/Dashboard.js
+++ data-visualization-dashboard/src/Components/Dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import logo from '../Assets/dashboard-visualization-logo.svg';
 import { useState, useEffect } from "react";
+import { BiHomeAlt, BiUser } from 'react-icons/bi';
+import { AiOutlineArrowLeft, AiOutlineBarChart, AiOutlineSetting } from 'react-icons/ai';
 import {
     Navbar,
     MobileNav,
@@ -9,7 +11,19 @@ import {
     Button,
     IconButton,
 } from "@material-tailwind/react";
+import { BsCalendar4, BsChatLeft, BsSearch } from 'react-icons/bs';
 const Dashboard = () => {
+    const [open, setOpen] = useState(true);
+    const Menus = [
+        { title: "Dashboard", src: <><BiHomeAlt ></BiHomeAlt></> },
+        { title: "Inbox", src: <><BsChatLeft></BsChatLeft></> },
+        { title: "Accounts", src: <><BiUser></BiUser></>, gap: true },
+        { title: "Schedule ", src: <><BsCalendar4></BsCalendar4></> },
+        { title: "Search", src: <><BsSearch></BsSearch></> },
+        { title: "Analytics", src: <><AiOutlineBarChart></AiOutlineBarChart></> },
+        // { title: "Files ", src: <><AiOutlineSetting></AiOutlineSetting></>, gap: true },
+        { title: "Setting", src: <><AiOutlineSetting></AiOutlineSetting></> },
+    ];
     const [openNav, setOpenNav] = useState(false);
 
     useEffect(() => {
@@ -18,6 +32,7 @@ const Dashboard = () => {
             () => window.innerWidth >= 960 && setOpenNav(false)
         );
     }, []);
+
     const navList = (
         <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
             <Typography
@@ -64,70 +79,55 @@ const Dashboard = () => {
     );
     return (
 
-        <div className='w-screen h-screen space-x-6 flex'>
-            <div class="flex flex-col items-center w-40 h-full overflow-hidden text-gray-700 bg-gray-100 rounded">
-                <a class="flex items-center w-full px-3 mt-3" href="/">
-                    <img src={logo} alt="" />
-                </a>
-                <div class="w-full px-2">
-                    <div class="flex flex-col items-center w-full mt-3 border-t border-gray-300">
-                        <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
-                            <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                            </svg>
-                            <span class="ml-2 text-sm font-medium">Dasboard</span>
-                        </a>
-                        <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
-                            <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                            </svg>
-                            <span class="ml-2 text-sm font-medium">Search</span>
-                        </a>
-                        <a class="flex items-center w-full h-12 px-3 mt-2 bg-gray-300 rounded" href="#">
-                            <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                            </svg>
-                            <span class="ml-2 text-sm font-medium">Insights</span>
-                        </a>
-                        <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
-                            <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
-                            </svg>
-                            <span class="ml-2 text-sm font-medium">Docs</span>
-                        </a>
-                    </div>
-                    <div class="flex flex-col items-center w-full mt-2 border-t border-gray-300">
-                        <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
-                            <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                            </svg>
-                            <span class="ml-2 text-sm font-medium">Products</span>
-                        </a>
-                        <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
-                            <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
-                            </svg>
-                            <span class="ml-2 text-sm font-medium">Settings</span>
-                        </a>
-                        <a class="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
-                            <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                            </svg>
-                            <span class="ml-2 text-sm font-medium">Messages</span>
-                            <span class="absolute top-0 left-0 w-2 h-2 mt-2 ml-2 bg-indigo-500 rounded-full"></span>
-                        </a>
-                    </div>
+        <div className="flex">
+            <div
+                className={` ${open ? "w-56" : "w-20 "
+                    } bg-gray-100 h-screen p-5  pt-8 relative duration-300`}
+            >
+                <AiOutlineArrowLeft
+                    className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+            border-2 rounded-full  ${!open && "rotate-180"}`}
+                    onClick={() => setOpen(!open)}
+                ></AiOutlineArrowLeft>
+                {/* <BiHomeAlt ></BiHomeAlt> */}
+                {/* <img
+            src="./src/assets/control.png"
+            className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+             border-2 rounded-full  ${!open && "rotate-180"}`}
+            onClick={() => setOpen(!open)}
+          /> */}
+                <div className="flex gap-x-4 items-center">
+                    <img src={logo} className={`cursor-pointer duration-500 ${open && "rotate-[360deg]"
+                        }`} alt="" />
+                    {/* <h1
+              className={`text-white origin-left font-medium text-xl duration-200 ${
+                !open && "scale-0"
+              }`}
+            >
+              Designer
+            </h1> */}
                 </div>
-                <a class="flex items-center justify-center w-full h-16 mt-auto bg-gray-200 hover:bg-gray-300" href="#">
-                    <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <span class="ml-2 text-sm font-medium">Account</span>
-                </a>
+                <ul className="pt-6">
+                    {Menus.map((Menu, index) => (
+                        <li
+                            key={index}
+                            className={`flex  rounded-md p-2 cursor-pointer hover:bg-blue-100 hover:ml-1 ease-in duration-100 text-gray-800 text-sm items-center gap-x-4 
+                ${Menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white"
+                                } `}
+                        >
+                            <span className='text-xl'>{Menu.src}</span>
+                            <span className={`${!open && "hidden"} origin-left duration-200`}>
+                                {Menu.title}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
             </div>
-            <div className="container mx-auto">
-                <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
-                    <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+            <div className="h-screen flex-1 p-7">
+                {/* <h1 className="text-2xl font-semibold ">Home Page</h1>
+           */}
+                <Navbar className="max-w-full  py-2 px-4 lg:px-8 lg:py-4">
+                    <div className="w-full flex items-center justify-between text-blue-gray-900">
                         <Typography
                             as="a"
                             href="#"
@@ -187,13 +187,14 @@ const Dashboard = () => {
                 </Navbar>
             </div>
         </div>
-
-
-        // <div className='bg-red-900'>
-
-        //     <Outlet></Outlet>
-        // </div>
     );
+
+
+    // <div className='bg-red-900'>
+
+    //     <Outlet></Outlet>
+    // </div>
+
 };
 
 export default Dashboard;
